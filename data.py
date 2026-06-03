1fef39b8b6671dcdf36151614a7f12991b2085da
Yash Goyal <yash77513@gmail.com>
2026-06-03 19:14:37 +0530

    Migrated to Alpha Vantage API

--- data.py
+++ data.py
@@ -1,31 +1,79 @@
 
-import yfinance as yf
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.fundamentaldata import FundamentalData
 
+import streamlit as st
+import pandas as pd
+
+api_key = st.secrets[
+    "ALPHA_VANTAGE_API_KEY"
+]
+
+ts = TimeSeries(
+    key=api_key,
+    output_format="pandas"
+)
+
+fd = FundamentalData(
+    key=api_key,
+    output_format="pandas"
+)
+
+@st.cache_data(ttl=3600)
 def get_company_data(stock):
 
-    company = yf.Ticker(stock)
+    overview, _ = fd.get_company_overview(
+        stock
+    )
 
-    info = company.info
-
-    return company, info
+    return overview
 
 
+@st.cache_data(ttl=3600)
 def get_stock_history(stock):
 
-    data = yf.download(
-        stock,
-        period="1y"
+    data, meta = ts.get_daily_adjusted(
+        symbol=stock,
+        outputsize="compact"
+    )
+
+    data = data.reset_index()
+
+    data.rename(
+        columns={
+            "date": "Date",
+            "4. close": "Close"
+        },
+        inplace=True
     )
 
     return data
 
 
-def get_financial_statements(company):
+@st.cache_data(ttl=3600)
+def get_financial_statements(stock):
 
-    financials = company.financials
+    income_statement, _ = (
+        fd.get_income_statement_annual(
+            stock
+        )
+    )
 
-    balance_sheet = company.balance_sheet
+    balance_sheet, _ = (
+        fd.get_balance_sheet_annual(
+            stock
+        )
+    )
 
-    cashflow = company.cashflow
+    cashflow, _ = (
+        fd.get_cash_flow_annual(
+            stock
+        )
+    )
 
-    return financials, balance_sheet, cashflow
+    return (
+        income_statement,
+        balance_sheet,
+        cashflow
+    )
+
